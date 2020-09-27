d9dfb964986a31192f6348d6f8ab44b0352bdc8a
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-09-27 21:34:53 +0200

    Solve exercise "Hello World" (cont.)
    Introduced some sanity.

--- hello-world/hello_world.sh
+++ hello-world/hello_world.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/env bash
 
-main () {
+# Sets option - exit script when there is an error:
+set -o errexit
+# Sets option - exit script when there are unset variables:
+set -o nounset
+
+hello_world() {
   echo "Hello, World!"
+  return 0
 }
 
-# call main with all of the positional arguments
+main () {
+  args=$@
+
+  if [ -z ${args} ]; then
+    hello_world
+    (( $? == 0 )) && exit 0
+    exit 1
+  else
+    echo "Too many command line parameters: ${args}"
+    exit 9
+  fi
+
+  exit 999
+}
+
+# Calls the main function
+#  passing to it all the positional arguments via "$@"
+#  (in quotes to prevent whitespace issues):
 main "$@"
